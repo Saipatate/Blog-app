d29579df5393e5b98a0fbfc45e02560ec4be6058
Saipatate <halgandewen44@gmail.com>
2023-03-29 23:33:39 +0200

    feat: updated main.tsx file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,18 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ArticlesContextProvider } from "./app/context/ArticleContext";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
       <BrowserRouter>
       <ArticlesContextProvider>
         <App />
         </ArticlesContextProvider>
       </BrowserRouter>
-    </QueryClientProvider>
   </React.StrictMode>
 );
